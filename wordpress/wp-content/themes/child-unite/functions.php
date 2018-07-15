f26c2a0752749a72e75cc09d611cc68b50c1a5be
Sadaf Siddiqui <sadaf_cu@hotmail.com>
2018-07-15 15:45:21 +0500

    1.4 and 1.5 done correctly
    1.4) add new type of post "Films". We need possibility to add new films.
    
    1.5) Add following taxonimies to films: Genre, Country, Year and Actors
    
    both are done in admin

--- wordpress/wp-content/themes/child-unite/functions.php
+++ wordpress/wp-content/themes/child-unite/functions.php
@@ -14,3 +14,87 @@ function theme_enqueue_styles() {
 //
 // Your code goes below
 //
+
+function create_post_type() {
+  register_post_type( 'sadaf_films',
+    array(
+      'labels' => array(
+        'name' => __( 'Films' ),
+        'singular_name' => __( 'Films' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+	  
+    )
+  );
+}
+add_action( 'init', 'create_post_type' );
+
+function themes_taxonomy() {  
+    register_taxonomy(  
+        'country',  //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces). 
+        'sadaf_films',        //post type name
+        array(  
+            'hierarchical' => true,  
+            'label' => 'Country',  //Display name
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'country', // This controls the base slug that will display before each term
+                'with_front' => false // Don't display the category base before 
+            )
+        )  
+    );  
+}  
+add_action( 'init', 'themes_taxonomy');
+
+function themes_taxonomy_genre() {  
+    register_taxonomy(  
+        'genre',  //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces). 
+        'sadaf_films',        //post type name
+        array(  
+            'hierarchical' => true,  
+            'label' => 'Genre',  //Display name
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'genre', // This controls the base slug that will display before each term
+                'with_front' => false // Don't display the category base before 
+            )
+        )  
+    );  
+}  
+add_action( 'init', 'themes_taxonomy_genre');
+
+function themes_taxonomy_year() {  
+    register_taxonomy(  
+        'year',  //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces). 
+        'sadaf_films',        //post type name
+        array(  
+            'hierarchical' => true,  
+            'label' => 'Year',  //Display name
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'year', // This controls the base slug that will display before each term
+                'with_front' => false // Don't display the category base before 
+            )
+        )  
+    );  
+}  
+add_action( 'init', 'themes_taxonomy_year');
+
+function themes_taxonomy_actors() {  
+    register_taxonomy(  
+        'actors',  //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces). 
+        'sadaf_films',        //post type name
+        array(  
+            'hierarchical' => true,  
+            'label' => 'Actors',  //Display name
+            'query_var' => true,
+            'rewrite' => array(
+                'slug' => 'actors', // This controls the base slug that will display before each term
+                'with_front' => false // Don't display the category base before 
+            )
+        )  
+    );  
+}  
+add_action( 'init', 'themes_taxonomy_actors');
+
